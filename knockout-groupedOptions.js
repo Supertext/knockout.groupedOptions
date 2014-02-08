8f2cbb100e6468219e13cb00915baae00e206b81
superusr <jameson@supertext.ch>
2014-02-08 19:37:30 +0100

    Update knockout-groupedOptions.js

--- knockout-groupedOptions.js
+++ knockout-groupedOptions.js
@@ -1,24 +1,65 @@
 /*!
- * knockout-groupedOption.js v1.0.0
+ * knockout-groupedOptions.js v0.1
  * 
  * Copyright (c) Andrew Jameson, www.Supertext.ch
  * Available under the MIT license: http://opensource.org/licenses/MIT
  */
+ 
+// data-bind="groupedSelect: { 
+//                              groups: { 
+//                                          coll: [groups collection  |  required; no default],
+//                                          label: [name of textual property  |  default: 'Label'],
+//                                          options: {
+//                                              coll: [name of the collection containing the <option> elements  |  default: 'Options']
+//                                              text: [name of the textual property  |  default: 'Text'],
+//                                              val: [name of the value property  |  default: 'Value']
+//                                          },
+//                                      }, 
+//                              optionsCaption: [text to be displayed for the default <option>, i.e., "Please select"  |  default: <option> not rendered],
+//								value: [the property (observable or otherwise) which references the selected <option> item.]
+//                          }"
+//
+// eg, data-bind="groupedSelect: { groups: { coll: TextTypes, label: 'Label', options: { coll: 'Options', text: 'Text', val: 'Value' } }, optionsCaption: '- Please select -' }"
+//
+// if using the conventions then this example may be shortened to
+// eg, data-bind="groupedSelect: { groups: { coll: TextTypes }, optionsCaption: '- Please select -' }"
+
+// Note that neither of the above examples considers a subscribing property, i.e., a property whose value is pre-selected and which is updated when the user selection changes.
 
 
-ko.bindingHandlers.groupedOption = {
-    update: function(element, valueAccessor) {
-
+ko.bindingHandlers.groupedOptions = {
+    "init": function (element, valueAccessor, allBindings) {
+        ko.utils.registerEventHandler(element, "change", function () {
+            var value = valueAccessor(),
+				property = ko.utils.domData.get(element, "property");
+            ko.utils.arrayForEach(element.getElementsByTagName("option"), function(node) {
+                if (node.selected) {
+					var data = ko.utils.domData.get(node, "data");
+					if (typeof(property) === "function") {
+						property(data);
+					} else if (typeof(property) === "string") {
+						var vm = ko.dataFor(element);
+						if (vm !== null) {
+							vm[property] = data;
+						}
+					}
+				}
+            });
+        });
+    },
+    "update": function(element, valueAccessor) {
+		
         // Get the parameters
 
         var h = ko.utils.unwrapObservable(valueAccessor());
 
         var groups = h["groups"],
             groupsCollection,
-            groupsLabel = "Label",
-            optionsCollProp = "Options",
-            optionsTextProp = "Text",
-            optionsValProp = "Value";
+            groupsLabel = "Label",			// the convention for this property
+            optionsCollProp = "Options",	// the convention for this property
+            optionsTextProp = "Text",		// the convention for this property
+            optionsValProp = "Value",		// the convention for this property
+			optionsValue = null;
 
         if (typeof (groups) === "undefined" || !groups) {
             throw "The \"groupedOption\" binding requires a \"groups\" object be specified.";
@@ -43,20 +84,15 @@ ko.bindingHandlers.groupedOption = {
                 optionsValProp = options["val"];
             }
         }
-
-
-        var defaultOpt = h["defaultOpt"],
-            defaultOptText,
-            defaultOptVal;
-        if (typeof (defaultOpt) !== "undefined" && defaultOpt) {
-            defaultOptText = defaultOpt["text"];
-            defaultOptVal = defaultOpt["val"];
-        }
-        // only specify a default value for 'defaultOptVal' if 'defaultOptText' has been specified
-        if ((typeof (defaultOptVal) !== "string" || !defaultOptVal || !defaultOptVal.length)
-            && (typeof (defaultOptText) === "string" && defaultOptText && defaultOptText.length)) {
-            defaultOptVal = "";
-        }
+		var selectedItem = h["value"],
+			selectedValue = ko.unwrap(selectedItem);
+		if (typeof(selectedItem) === "function") {
+			ko.utils.domData.set(element, "property", selectedItem);	// this records the subscribing property, i.e., the property which stores the selected item
+		} else if (typeof(selectedItem) === "string") {
+			// this caters for the situation whereby the subscribing property is not an observable
+			ko.utils.domData.set(element, "property", selectedItem);	// this records the name of the subscribing property, i.e., the property which stores the selected item
+		}
+		
 
         // find how many elements have already been added to 'element'
         var childCount = 0,
@@ -74,10 +110,10 @@ ko.bindingHandlers.groupedOption = {
         // if 'element' is currently empty then add the default <option> element
         if (!childCount) {
 
-            if (typeof(defaultOptText) === "string" && defaultOptText && defaultOptText.length) {
+			var defaultText = h["optionsCaption"];
+            if (defaultText && typeof(defaultText) === "string" && defaultText.length) {
                 var defaultOption = document.createElement("option");
-				defaultOption.setAttribute("value", defaultOptVal);
-                defaultOption.innerHTML = defaultOptText;
+                defaultOption.innerHTML = defaultText;
                 element.appendChild(defaultOption);
             }
         } else {
@@ -108,7 +144,8 @@ ko.bindingHandlers.groupedOption = {
             var options = ko.utils.unwrapObservable(coll[childCount][optionsCollProp]);
             for (var j = 0, jMax = options.length; j < jMax; j++) {
 
-                var optionText = ko.utils.unwrapObservable(options[j][optionsTextProp]);
+				var thisOption = options[j],
+					optionText = ko.utils.unwrapObservable(thisOption[optionsTextProp]);
 
                 // if there is no text for this <option> then don't add the <option>
                 if (!optionText || !optionText.length) {
@@ -119,10 +156,18 @@ ko.bindingHandlers.groupedOption = {
                 option.innerHTML = optionText;
 
                 // add the 'value' attribute if it exists
-                var val = ko.utils.unwrapObservable(options[j][optionsValProp]);
+                var val = ko.utils.unwrapObservable(thisOption[optionsValProp]);
                 if (val && val.length) {
                     option.setAttribute("value", val);
                 }
+				
+				// if this is the same object as the 'value' parameter then indicate so				
+				if (thisOption === selectedValue) {
+					option.setAttribute("selected", "selected");
+				}
+				
+				// add the observable to this node so that we may retrieve this data in future
+				ko.utils.domData.set(option, "data", thisOption);
 
                 // now add this <option> to the parent <optgroup>
                 optGroup.appendChild(option);
